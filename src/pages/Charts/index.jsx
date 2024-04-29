acb02695a7959e9aa0b6c1678e1c7677cf098fa5
isaiasleite <isaiasleite230@gmail.com>
2024-04-29 13:48:08 -0400

    gráfico

--- src/pages/Charts/index.jsx
+++ src/pages/Charts/index.jsx
@@ -1,26 +1,17 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Cookies from 'universal-cookie';
+import DrawerChart from '../../components/DrawerChart';
 
 const Charts = () => {
-    const navigate = useNavigate();
-
-    const cookies = new Cookies();
-    const token = cookies.get('token');
-
-    useEffect(() => {
-        if (!token) {
-            navigate('/');
-        }
-    }, []);
-
     return (
         <div className="m-5">
             <h1>Gráficos</h1>
-
             <p>
                 <a className="btn btn-secondary" href={`/home`}>Página Inicial</a>
             </p>
+            <div>
+                <h2>Relação de status de usuários</h2>
+                <DrawerChart />
+            </div>
         </div>
     );
 };
